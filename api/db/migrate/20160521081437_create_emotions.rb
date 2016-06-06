3d17f8c89b1a22868e98f02f86dd66159eeceb71
toririn <toririn.paftako@gmail.com>
2016-06-07 00:54:23 +0900

    fix emotion table

--- api/db/migrate/20160521081437_create_emotions.rb
+++ api/db/migrate/20160521081437_create_emotions.rb
@@ -3,8 +3,8 @@ class CreateEmotions < ActiveRecord::Migration[5.0]
     create_table :emotions, id: false do |t|
       t.column :id, 'BIGINT PRIMARY KEY AUTO_INCREMENT'
       t.integer :chat_id, limit: 8
-      t.float :emotion
-      t.integer :level_id
+      t.float :emovalue
+      t.text :repeat
       t.timestamps
     end
   end
