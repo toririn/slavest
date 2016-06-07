d89027dea4bc5c950864e3d2c1db3a3a67f4af6a
toririn <toririn.paftako@gmail.com>
2016-06-07 10:20:07 +0900

    relationテーブルの作成条件

--- api/app/models/chat.rb
+++ api/app/models/chat.rb
@@ -1,3 +1,8 @@
 class Chat < ApplicationRecord
   #"channel"=>"C14TGRC1G", "user"=>"U0P75C2F8", "text"=>"@sv\nへい", "ts"=>"1465219725.000002", "team"=>"T0P71EC2H", "api_token"=>"helloworld",
+  after_create :create_relation_chat_mention_user
+
+  def create_relation_chat_mention_user
+    # TODO chat 登録時にrelationテーブルのレコードも作成する
+  end
 end
