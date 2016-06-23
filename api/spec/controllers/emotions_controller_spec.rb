24a9c376509c098e00d2a83bac7f08793dbeb20b
toririn <toririn.paftako@gmail.com>
2016-06-23 22:43:14 +0900

    add emotions controller #average test

--- api/spec/controllers/emotions_controller_spec.rb
+++ api/spec/controllers/emotions_controller_spec.rb
@@ -23,16 +23,63 @@ describe EmotionsController do
         expect(res["result"]).to eq true
       end
 
-      # pend
-      #let(:work_args) { { slack_user_id: user.slack_id, slack_channel_id: channel.slack_id, text: "#{Time.zone.now}-rspec-test", ts: "00000000.000000" } }
-      #it 'Jobが登録されること' do
-      #  get_emotion_job = {
-      #    job: Everests::GetEmotionJob,
-      #    args: [work_args],
-      #    at: Time.zone.now.to_i
-      #  }
-      #  assert_enqueued_with(get_emotion_job) { job.perform(work_args) }
-      #end
+      let(:work_args) { { slack_user_id: user.slack_id, slack_channel_id: channel.slack_id, text: "#{Time.zone.now}-rspec-test", ts: "00000000.000000" } }
+      it 'Jobが登録されること' do
+        get_emotion_job = {
+          job: Everests::GetEmotionJob,
+          args: [work_args],
+        }
+        assert_enqueued_with(get_emotion_job) { Everests::GetEmotionJob.perform_later(work_args) }
+      end
+    end
+
+    context '異常系:' do
+      before do
+        post :create, params: { user: user.slack_id.concat("missing"), channel: channel.slack_id, text: "test", ts: "0000000000", api_token: EasySettings.slavest.api_token }
+      end
+
+      it 'DBに存在しないユーザの投稿があったら警告を返す' do
+        res = JSON.parse(response.body)
+        expect(res["message"]).to eq EasySettings.controllers.emotions.messages.user_not_found
+      end
+
+      it 'statu:200, result:false のJSONが返ること' do
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq 200
+        expect(res["result"]).to eq false
+      end
+    end
+  end
+
+  describe 'POST #average' do
+    let(:user)    { create(:user) }
+    let(:channel) { create(:channel) }
+    let(:job)     { Everests::GetEmotionJob.new }
+
+    context '正常系:' do
+      before do
+        post :average, params: { user: user.slack_id, channel: channel.slack_id, text: "@slarest\naverage,6/1,6/2", ts: "0000000000", api_token: EasySettings.slavest.api_token }
+      end
+      it '200 OK が返ること' do
+        expect(response).to be_success
+        expect(response.status).to eq 200
+      end
+
+      it 'message:OK, statu:200, result:true のJSONが返ること' do
+        res = JSON.parse(response.body)
+        expect(res["message"]).to eq EasySettings.controllers.emotions.messages.ok
+        expect(res["status"]).to eq 200
+        expect(res["result"]).to eq true
+      end
+
+      let(:work_args) { { slack_user_id: user.slack_id, slack_channel_id: channel.slack_id, text: "@slarest\naverage,6/1,6/2", ts: "00000000.000000" } }
+      it 'Jobが登録されること' do
+        get_emotion_job = {
+          job: Everests::PostAverageEmovalueJob,
+          args: [work_args],
+        }
+        assert_enqueued_with(get_emotion_job) { Everests::PostAverageEmovalueJob.perform_later(work_args) }
+      end
     end
 
     context '異常系:' do
