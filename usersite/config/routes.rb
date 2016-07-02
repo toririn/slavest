04ff8dc2c41e68ac010a5f3bad4047cf007e41c0
toririn <toririn.paftako@gmail.com>
2016-07-02 21:29:52 +0900

    add views

--- usersite/config/routes.rb
+++ usersite/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root controller: :sessions, action: :index
   resources :sessions, only: [:create]
-  resources :users
+  resources :users, only: [:index, :show] do
+    resources :emovalue_options, only: [:index, :edit] do
+      member do
+        post :edit
+      end
+    end
+  end
 end
